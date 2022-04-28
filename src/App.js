98ac7e0d82246769b0458f1f2b8e394320025edd
Aaronokwok <aaronkwok101@gmail.com>
2022-04-27 17:26:47 -0700

    added state keeping functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react"; 
 import { nanoid } from "nanoid"; 
 import Die from "./Die"; 
+//import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
 export default function App() {
-    const [dice, setDice] = React.useState(allNewDice())
+    const [dice, setDice] = React.useState(allNewDice());
+    const [allHeld, setAllHeld] = React.useState(false);
+
+    //useEffect to use an event listener that watches every time dice change
+    React.useEffect(() => {
+        if (dice.every(die => die.value === dice[0].value && die.isHeld === true)) {
+           setAllHeld(true)
+        }
+    }, [dice])
 
     //Generate new Dice 
     function generateNewDie() {
@@ -14,27 +24,53 @@ export default function App() {
         }
     }
 
-    //All new dice on first render
+    //All new dice on first render and on "roll" button click
     function allNewDice() {
-        const newDice = []; 
+        const newDice = []
         for (let i = 0; i < 10; i++) {
             newDice.push(generateNewDie())
         }
-        return newDice; 
+        return newDice;
     }
 
     //roll new dice
     function rollNewDice() {
-        return setDice(allNewDice())
+        if (allHeld) {
+            setAllHeld(false)
+            setDice(allNewDice())
+        }
+        setDice(prevDice => {
+            return prevDice.map(die => {
+               return die.isHeld ? die : generateNewDie()
+            })
+        })
     }
 
+    //hold die
+    function holdDie(id) {
+        setDice(prevDice => (
+             prevDice.map(die => {
+                if (die.id === id) {
+                    return {
+                        ...die, 
+                        isHeld: !die.isHeld
+                    }
+                } else {
+                    return die;
+                }
+            })
+        ))
+    }
+    
+
     //diceElement components from Die.js
-    const diceElements = dice.map(die => {
+    const diceElements = dice.map((die, index) => {
         return <Die 
-            key={die.id}
+            key={index}
+            id={die.id}
             value={die.value}
             isHeld={die.isHeld}
-            roll={rollNewDice}
+            holdDie={() => holdDie(die.id)}
         />
     })
 
@@ -49,8 +85,9 @@ export default function App() {
                 className="rollDice"
                 onClick={rollNewDice}
             >
-                Roll
+                {allHeld ? "New Game" : "Roll"}
             </button>
+            {allHeld ? <Confetti /> : null}
         </main>
     )
 }

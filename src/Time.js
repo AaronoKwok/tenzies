5932962b99a536d2ba48af790c9062bad72ab4ad
Aaronokwok <aaronkwok101@gmail.com>
2022-10-20 10:05:58 -0700

    time

--- src/Time.js
+++ src/Time.js
@@ -1,12 +1,11 @@
 import React from "react"; 
 
-export default function Time(props) {
+export default function Time(initial) {
     return (
         <div>
                 <span className="stopWatch">Duration of Play: </span>
-                <span>{("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:</span>{/* 60000ms/min */}
-                <span>{("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}</span>{/* 1000ms/s */}
-                {/*<span>{("0" + ((time /10) % 100)).slice(-2)}</span> {/* divided ms by 10 to move decimal point to left so that you can see numbers change on screen */}              
+                <span>{("0" + Math.floor((initial.time / 60000) % 60)).slice(-2)}:</span>{/* 60000ms/min */}
+                <span>{("0" + Math.floor((initial.time / 1000) % 60)).slice(-2)}</span>{/* 1000ms/s */}
         </div>
     )
 }

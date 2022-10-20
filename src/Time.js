e3236fbfe552fea3fbcbf1e3b0590be8dfac10ea
Aaronokwok <aaronkwok101@gmail.com>
2022-10-20 10:23:09 -0700

    time.js destructured assign

--- src/Time.js
+++ src/Time.js
@@ -1,11 +1,11 @@
 import React from "react"; 
 
-export default function Time(initial) {
+export default function Time({time}) {
     return (
         <div>
                 <span className="stopWatch">Duration of Play: </span>
-                <span>{("0" + Math.floor((initial.time / 60000) % 60)).slice(-2)}:</span>{/* 60000ms/min */}
-                <span>{("0" + Math.floor((initial.time / 1000) % 60)).slice(-2)}</span>{/* 1000ms/s */}
+                <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>{/* 60000ms/min */}
+                <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>{/* 1000ms/s */}
         </div>
     )
 }
